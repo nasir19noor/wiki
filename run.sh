98c40f8edac13db8b49201bc1977300b9fc26d63
Nasir Noor <nasir19noor@gmail.com>
2026-03-04 22:27:51 +0700

    update

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
-# docker compose down
-# docker compose up -d --build
-# docker compose logs -f docmost
+docker compose down
+docker compose build --no-cache
+docker compose up -d
+docker compose logs -f docmost
 
-docker stop docmost && docker rm docmost
-docker build -t docmost:latest .
-docker run -d \
-  --network host \
-  -e PORT="3001" \
-  -e DATABASE_URL \
-  -e ADMIN_PASSWORD \
-  --env-file .env \
-  --name docmost docmost:latest
+# docker stop docmost && docker rm docmost
+# docker build -t docmost:latest .
+# docker run -d \
+#   --network host \
+#   -e PORT="3001" \
+#   -e DATABASE_URL \
+#   -e ADMIN_PASSWORD \
+#   --env-file .env \
+#   --name docmost docmost:latest
